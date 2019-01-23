224691d052e1f35fd813f61a9cb5867a73d9b8c6
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-01-23 13:16:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs')
+const if = require('if')
 var prefix = "#";
 
 client.on('ready', () => {
@@ -152,30 +153,7 @@ if (err) console.error(err);
 })
       })
  
-client.on('ready',async () => {
-setInterval(function(){
-var currentTime = new Date(),
-hours = currentTime.getHours() + 3 ,
-ReBeeL = currentTime.getMinutes(),
-ReBeeeL = currentTime.getSeconds(),
-Codes = currentTime.getFullYear(),
-CodeS = currentTime.getMonth() + 1,
-CoDeS = currentTime.getDate()
-if (ReBeeL < 10) {
-ReBeeL = "0" + ReBeeL;
-}
-var suffix = "AM";
-if (hours >= 12) {
-suffix = "PM";
-hours = hours - 12;
-}
-if (hours == 0) {
-hours = 12;
-}
-client.channels.find('id', '532954636672434190').setName(`Time - ${hours} : ${ReBeeL} : ${ReBeeeL} ${suffix}`) 
-client.channels.find('id', '532954636672434190').setName(`Date : ${Codes} - ${CodeS} - ${CoDeS}`)
-}, 1000);
-});
+
 
 
 client.on("guildMemberAdd", member => {
