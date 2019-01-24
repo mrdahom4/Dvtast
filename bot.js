17d7d15eb02554a4e5b5b6d7ff61f7954beaec81
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-01-24 14:21:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -178,34 +178,14 @@ welcomer.sendFile(canvas.toBuffer())
  }
 });
 
-const invites = {};
-const wait = require('util').promisify(setTimeout);
-client.on('ready', () => {
-  wait(1000);
- 
-  client.guilds.forEach(g => {
-    g.fetchInvites().then(guildInvites => {
-      invites[g.id] = guildInvites;
-    });
-  });
-});
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    invites[member.guild.id] = guildInvites;
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const logChannel = member.guild.channels.find(channel => channel.name === "welcome");
-    logChannel.send(`**Invited by: <@${inviter.id}>**`);
-  });
-});
+
 client.on('guildMemberAdd', member => {
   
   const channel = member.guild.channels.find(ch => ch.name === 'welcome');
  
   if (!channel) return;
 
-  channel.send(`**Welcome to ${member.guild.name}, ${member}**`);
+  channel.send(`**Invited by: <@${inviter.id}>**`);
 });
 
 
