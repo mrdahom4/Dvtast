2c8bbc04336ec9c97f11cb4d044476fc5753de34
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-01-24 12:23:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -166,6 +166,17 @@ welcomer.sendFile(canvas.toBuffer())
       });                    
  }
 });
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const logChannel = member.guild.channels.find(channel => channel.name === "simo");
+    logChannel.send(`**Invited by: <@${inviter.id}>**`);
+  });
+});
+
 
 client.on('message' , async (message) => {
  if (message.content.startsWith(prefix + 'w')) {
