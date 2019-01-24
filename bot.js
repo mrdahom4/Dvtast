600889a726714a7f88fe839eced4bf8f15a38188
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-01-24 12:18:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -167,4 +167,21 @@ welcomer.sendFile(canvas.toBuffer())
  }
 });
 
+client.on('message' , async (message) => {
+ if (message.content.startsWith(prefix + 'w')) {
+  const args = message.content.substring(prefix.length).split(' ');
+
+ message.delete();
+args.shift() 
+let msg = args.join(' ') 
+message.channel.createWebhook(message.author.username, message.author.avatarURL) 
+    .then(wb => {
+        const user = new Discord.WebhookClient(wb.id, wb.token) 
+        user.send(msg); 
+        user.delete() 
+    })
+    .catch(console.error)
+ }
+});
+
 client.login(process.env.BOT_TOKEN);
