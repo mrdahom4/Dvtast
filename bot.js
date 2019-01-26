ccde4844304baac84804be3c4a280a22f4133294
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-01-26 07:19:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -256,7 +256,7 @@ welcomer.sendFile(canvas.toBuffer())
 
 
 client.on('ready', () => {
-  wait(1000);
+  wait(1005);
 
   client.guilds.forEach(g => {
     g.fetchInvites().then(guildInvites => {
@@ -270,7 +270,7 @@ client.on('guildMemberAdd', member => {
     const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
     const inviter = client.users.get(invite.inviter.id);
     const Galal = member.guild.channels.find("name", "welcome");
-     Galal.send(`**<@${member.user.id}> joined by** <@${inviter.id}>**`);
+     Galal.send(`<@${member.user.id}> **joined by** <@${inviter.id}>`);
    //  Galal.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
   }); 
 });
